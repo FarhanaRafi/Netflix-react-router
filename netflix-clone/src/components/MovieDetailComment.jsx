7ffce9989ed007135a00386828d8d441c6216a0d
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-02-08 16:48:19 +0100

    button done

--- netflix-clone/src/components/MovieDetailComment.jsx
+++ netflix-clone/src/components/MovieDetailComment.jsx
@@ -1,46 +1,14 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-function MovieDetailComment() {
-  const params = useParams();
-  console.log(params, "params");
-  const [commentToShow, setCommentToShow] = useState(null);
-
-  const fetchComment = async () => {
-    let response = await fetch(
-      "http://www.omdbapi.com/?apikey=bcef6f96&i=" + params.movieId.Ratings
-    );
-    if (response.ok) {
-      let data = await response.json();
-      console.log(data, "data");
-      setCommentToShow(data);
-    }
-    console.log(response);
-  };
-  useEffect(() => {
-    let foundComment = fetchComment;
-
-    console.log("movie comment found", foundComment);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+function MovieDetailComment(props) {
   return (
-    <>
-      {commentToShow ? (
-        <>
-          <Container>
-            <li>
-              {commentToShow.Source}- {commentToShow.Value}
-            </li>
-          </Container>
-        </>
-      ) : (
-        ""
-      )}
-    </>
+    <ListGroup>
+      {props.comments.map((comment) => {
+        return;
+      })}
+    </ListGroup>
   );
 }
 
--- netflix-clone/src/components/MovieDetails.jsx
+++ netflix-clone/src/components/MovieDetails.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Spinner, Card, Container } from "react-bootstrap";
+import { Spinner, Card, Container, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 // import AllCards from "./AllCards";
 
@@ -9,6 +9,7 @@ const MovieDetails = (props) => {
   const params = useParams();
   console.log(params, "params");
   const [movieToShow, setMovieToShow] = useState(null);
+  const [comments, setComments] = useState([]);
 
   const fetchMovie = async () => {
     let response = await fetch(
@@ -24,11 +25,30 @@ const MovieDetails = (props) => {
 
   useEffect(() => {
     let foundMovieObject = fetchMovie();
+    let commentFound = movieComments();
+    console.log(commentFound);
 
     console.log("movie details found", foundMovieObject);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const movieComments = async () => {
+    let res = await fetch(
+      "https://striveschool-api.herokuapp.com/api/comments/" + params.movieId,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzQ1ZmU3MzczODAwMTUzNzQzNzgiLCJpYXQiOjE2NzU4NzA1OTEsImV4cCI6MTY3NzA4MDE5MX0.IG1txVxlAgWxVvwDUIvso4JjeaAtEaHdVlWNR0j90iI",
+        },
+      }
+    );
+    if (res.ok) {
+      let data = await res.json();
+      console.log(data);
+      setComments(data);
+    }
+  };
 
   return (
     <>
@@ -38,20 +58,31 @@ const MovieDetails = (props) => {
             <h2 className="text-white text-center">
               Details of {movieToShow.Title}
             </h2>
-            <Card className="mb-3 mx-5">
-              <Card.Img variant="top" src={movieToShow.Poster} height={800} />
-              <Card.Body>
-                <Card.Title>
-                  {movieToShow.Title} {movieToShow.Type} -{" "}
-                  {movieToShow.Language} - {movieToShow.Country}
-                </Card.Title>
-                <Card.Text>
-                  {movieToShow.Type} - {movieToShow.Language} -{" "}
-                  {movieToShow.Country}
-                </Card.Text>
-                <Card.Text>{movieToShow.Awards}</Card.Text>
-              </Card.Body>
-            </Card>
+            <Row>
+              <Col>
+                <Card className="mb-3 mx-5">
+                  <Card.Img
+                    variant="top"
+                    src={movieToShow.Poster}
+                    height={800}
+                  />
+                  <Card.Body>
+                    <Card.Title>
+                      {movieToShow.Title} {movieToShow.Type} -{" "}
+                      {movieToShow.Language} - {movieToShow.Country}
+                    </Card.Title>
+                    <Card.Text>
+                      {movieToShow.Type} - {movieToShow.Language} -{" "}
+                      {movieToShow.Country}
+                    </Card.Text>
+                    <Card.Text>{movieToShow.Awards}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              {/* <Col>
+              {comments.length? }
+              </Col> */}
+            </Row>
           </Container>
         </>
       ) : (
